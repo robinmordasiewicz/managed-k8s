86a21cc8debdfb2c86104a710bbe5e0e6b169e27
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-04-30 23:24:10 -0400

    small changes

--- versions.tf
+++ versions.tf
@@ -11,10 +11,8 @@ terraform {
     }
   }
   cloud {
-    #organization = "${var.tfc_organization}"
     organization = "bankexample"
     workspaces {
-      #name = "${var.tfc_k8s_workspace_name}"
       name = "k8s"
     }
   }
