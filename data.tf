4f2477b15968a8663211f42cfac1056ffb3abbdb
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-04-30 23:20:36 -0400

    small changes

--- data.tf
+++ data.tf
@@ -2,10 +2,9 @@ data "terraform_remote_state" "remote_state" {
   backend = "remote"
 
   config = {
-    #organization = "${var.tfc_organization}"
+     #defaults = 
     organization = "bankexample"
     workspaces = {
-      #name = "${var.tfc_k8s_workspace_name}"
       name = "k8s"
     }
   }
