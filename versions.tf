17b7f42baed24ffff95302a4302745b678f96afc
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-04-30 23:25:45 -0400

    small changes

--- versions.tf
+++ versions.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_version = ">= 0.14.11"
   required_providers {
     volterra = {
       source  = "volterraedge/volterra"
-      version = ">= 0.11.19"
+      version = ">= 0.11.21"
     }
     libvirt = {
       source  = "dmacvicar/libvirt"
