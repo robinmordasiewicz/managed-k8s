ab40972c9f7e4cbbd18a36e7409db7b638b3421a
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-05-09 18:41:57 -0400

    small changes

--- data.tf
+++ data.tf
@@ -1,11 +1,11 @@
-data "terraform_remote_state" "remote_state" {
-  backend = "remote"
-
-  config = {
-     #defaults = 
-    organization = "bankexample"
-    workspaces = {
-      name = "k8s"
-    }
-  }
-}
+#data "terraform_remote_state" "remote_state" {
+#  backend = "remote"
+#
+#  config = {
+#     #defaults = 
+#    organization = "bankexample"
+#    workspaces = {
+#      name = "k8s"
+#    }
+#  }
+#}
--- providers.tf
+++ providers.tf
@@ -1,12 +1,12 @@
 provider "volterra" {
-  api_p12_file = var.VOLT_API_P12_FILE
-  #api_p12_file     = "${var.tenant}.ves.volterra.io.api-creds.p12"
-  #  api_cert          = var.VOLT_API_CERT != "" ? "" : var.VOLT_API_CERT
-  #  api_key           = var.VOLT_API_KEY != "" ? "" : var.VOLT_API_KEY
+  # api_p12_file = var.VOLT_API_P12_FILE
+  # api_p12_file     = "${var.tenant}.ves.volterra.io.api-creds.p12"
+  # api_cert          = var.VOLT_API_CERT != "" ? "" : var.VOLT_API_CERT
+  # api_key           = var.VOLT_API_KEY != "" ? "" : var.VOLT_API_KEY
   # enterprise api url
-  #url = "https://${var.tenant}.console.ves.volterra.io/api"
+  # url = "https://${var.tenant}.console.ves.volterra.io/api"
   # free api url
-  url              = "https://console.ves.volterra.io/api"
+  # url              = "https://console.ves.volterra.io/api"
 }
 
 provider "libvirt" {
