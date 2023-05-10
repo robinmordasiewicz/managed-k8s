4f627e00c166e21cb09cd6aec211d07ef80b0c52
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-05-09 23:09:56 -0400

    small changes

--- volterraedge/providers.tf
+++ volterraedge/providers.tf
@@ -1,6 +1,6 @@
 provider "volterra" {
-  #api_p12_file = var.VOLT_API_P12_FILE
-  api_p12_file = "volterra.ves.volterra.io.api-creds.p12"
+  api_p12_file = var.VOLT_API_P12_FILE
+  #api_p12_file = "volterra.ves.volterra.io.api-creds.p12"
   #url          = "https://${var.tenant}.console.ves.volterra.io/api"
   url = var.url
 }
