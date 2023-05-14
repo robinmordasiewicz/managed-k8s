4fdad0f2f29a889fac3654ead424a051ff279034
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-05-13 22:02:45 -0400

    update resume

--- libvirt/providers.tf
+++ libvirt/providers.tf
@@ -1,4 +1,4 @@
 provider "libvirt" {
-  uri = "qemu+ssh://${var.libvirtadmin}@${var.libvirtip}/system?sshauth=privkey&no_verify=1"
-  #uri = "qemu:///system"
+  #uri = "qemu+ssh://${var.libvirtadmin}@${var.libvirtip}/system?sshauth=privkey&no_verify=1"
+  uri = "qemu:///system"
 }
